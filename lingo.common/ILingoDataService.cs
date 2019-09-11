12e1097b2f81853a37a34ae1390917c23a0bf02f
Stu McKenzie <stu.mckenzie@gmail.com>
2019-09-11 17:11:36 +1200

    adding ability to fetch list of available groups from ILingoDataService

--- lingo.common/ILingoDataService.cs
+++ lingo.common/ILingoDataService.cs
@@ -6,6 +6,12 @@ namespace lingo.common
     public interface ILingoDataService
     {
         /// <summary>
+        /// Iterates the available groups. These might be filenames
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<(string groupKey, string groupName)> IterAvailableGroups();
+
+        /// <summary>
         /// Fetches new base-phrases from server, merges any updates, dirties any existing phrases, and pushes new local translations to the server.
         /// If any base phrases have changed since before then those translations will be marked as dirty.
         /// After merging, translations get published to server.
